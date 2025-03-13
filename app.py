6ef3e7e35bdc79c348a46a891e5f6d9e7b0a04d3
Mohamed Elghannam <82755084+moghanam66@users.noreply.github.com>
2025-03-13 13:44:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,13 @@ async def messages(req: web.Request) -> web.Response:
 
     await adapter.process_activity(activity, auth_header, handle_message_activity)
     return web.Response(status=200)
-
-app = web.Application()
-app.router.add_post("/api/messages", messages)
+def init_func(argv):
+    app = web.Application()
+    app.router.add_post("/api/messages", messages)
+    return app
 
 if __name__ == "__main__":
     import os
+    app = init_func(None)
     port = int(os.environ.get("PORT", 3978))
     web.run_app(app, host="0.0.0.0", port=port)
